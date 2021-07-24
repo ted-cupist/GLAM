f450b5c8b3e3e8bff09bae17a05bedbb8cbec371
정성훈 <52942411+jsh0128@users.noreply.github.com>
2021-07-24 22:41:31 +0900

    DELETE:useSelected delete

--- src/hooks/useSelected.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const useSelected = () => {
-  const [selected, setSelected] = useState<number>(0);
-
-  return { selected, setSelected };
-};
-
-export default useSelected;
